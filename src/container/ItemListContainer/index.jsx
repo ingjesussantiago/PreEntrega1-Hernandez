5620f604cc6af9a0820f31dfd415149cedae9970
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2022-12-21 10:37:53 -0600

    reestare lo que no me salio title2

--- src/container/ItemListContainer/index.jsx
+++ src/container/ItemListContainer/index.jsx
@@ -5,7 +5,7 @@ import "./styles.scss"
 const ItemListContainer = () => {
   return (
     <div className='ItemCard'>
-      <Item title={"Samurai 1"}/>
+      <Item title={"Samurai 1"} title2={"ok"}/>
       <Item title={"Samurai 2"}/>
       <Item title={"Samurai 3"}/>
       <Item title={"Samurai 4"}/>
