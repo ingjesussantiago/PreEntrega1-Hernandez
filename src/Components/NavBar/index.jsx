51c18f51630fcc8f8fc25a116245d671bb5d7f79
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2022-12-21 11:14:18 -0600

    terminado

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">Mi Tienda</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -17,9 +17,9 @@ const NavBar = () => {
               <a className="nav-link active" aria-current="page" href="/#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="#">Fotos</a>
             </li>
-            <li className="nav-item dropdown">
+            {/* <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Dropdown
               </a>
@@ -29,10 +29,10 @@ const NavBar = () => {
                 <li><hr className="dropdown-divider" /></li>
                 <li><a className="dropdown-item" href="/#">Something else here</a></li>
               </ul>
-            </li>
-            <li className="nav-item">
+            </li> */}
+            {/* <li className="nav-item">
               <a className="nav-link disabled">Disabled</a>
-            </li>
+            </li> */}
             <li>
             </li>
           </ul>
